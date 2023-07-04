10bb16c3ae5fc42c55e78390cfc68b5c6e79682d
CJ Young <chrjyoung@gmail.com>
2023-07-04 15:39:18 -0400

    Add "play" and "analyze" button links to homepage

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,23 +1,25 @@
 import Navbar from "../components/Navbar";
-import { useAuthContext } from "../contexts/AuthContext";
+import playImage from "../assets/play-button-01.svg";
+import analyzeImage from "../assets/analyze-button-01.svg";
+import "../styles/Home.scss";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  const { updateUsername } = useAuthContext();
   return (
-    <>
+    <div className="home">
       <Navbar />
-      <div className="home">
-        <button className="play" aria-label="play"></button>
-        <button
-          className="analyze"
-          aria-label="analyze"
-          onClick={() => {
-            updateUsername("Thisuser");
-          }}
-        >
-          ANALYZE
-        </button>
+      <div className="home__main">
+        <Link to="/play">
+          <button className="play" aria-label="play">
+            <img src={playImage} alt="Play" />
+          </button>
+        </Link>
+        <Link to="/analyze">
+          <button className="analyze" aria-label="analyze">
+            <img src={analyzeImage} alt="Analyze" />
+          </button>
+        </Link>
       </div>
-    </>
+    </div>
   );
 }
