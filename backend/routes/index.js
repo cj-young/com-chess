44831637fd0153130a8735a5a62e69093787ff8f
CJ Young <chrjyoung@gmail.com>
2023-07-25 15:32:56 -0400

    Remove name route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,8 +4,5 @@ const authRoutes = require("./authRoutes");
 const router = express.Router();
 
 router.use("/auth", authRoutes);
-router.get("/name", (req, res, next) => {
-  res.json({ username: req.user.username });
-});
 
 module.exports = router;
