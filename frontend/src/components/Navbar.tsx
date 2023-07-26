09f2f77624a9bedaa254daa10a719fdaec79aecf
CJ Young <chrjyoung@gmail.com>
2023-07-26 17:35:38 -0400

    Remove useState from imports

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import logo from "../assets/logo-light.svg";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.scss";
