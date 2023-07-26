e2e329674f264592e68d86845eb260603fa8e29c
CJ Young <chrjyoung@gmail.com>
2023-07-26 15:08:08 -0400

    Create game page component

---
+++ frontend/src/pages/Game.tsx
@@ -0,0 +1,10 @@
+import Navbar from "../components/Navbar";
+
+export default function Game() {
+  return (
+    <div className="game">
+      <Navbar />
+      <div className="game__container"></div>
+    </div>
+  );
+}
