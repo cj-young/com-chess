d2852ee59a8eb5328f63244f4aadd9c32e6708db
CJ Young <chrjyoung@gmail.com>
2023-07-26 21:01:46 -0400

    Replace PlayHome page with Play

--- frontend/src/pages/Play.tsx
+++ frontend/src/pages/Play.tsx
@@ -1,5 +1,20 @@
-import PlayHome from "./PlayHome";
+import Navbar from "../components/Navbar";
+import playBot from "../assets/play-bot-button-01.svg";
+import playFriend from "../assets/play-friend-button-01.svg";
+import "../styles/Play.scss";
 
 export default function Play() {
-  return <PlayHome />;
+  return (
+    <div className="play-home">
+      <Navbar />
+      <div className="play-home__main">
+        <button className="play-friend">
+          <img src={playFriend} alt="Play friend" />
+        </button>
+        <button className="play-bot">
+          <img src={playBot} alt="Play bot" />
+        </button>
+      </div>
+    </div>
+  );
 }
