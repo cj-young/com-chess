2c0e0a3ad968a426ab3c892493d3fe71bb56b95c
CJ Young <chrjyoung@gmail.com>
2023-07-30 18:03:51 -0400

    Add initial notification loading

--- frontend/src/components/Notifications.tsx
+++ frontend/src/components/Notifications.tsx
@@ -6,17 +6,28 @@ import { socket } from "../config/socket";
 import MessageNotification from "./MessageNotification";
 import { useUserContext } from "../contexts/UserContext";
 
+type Notification =
+  | {
+      type:
+        | "friendRequest"
+        | "friendAccept"
+        | "friendWasDeclined"
+        | "friendDidDecline";
+      from: string;
+    }
+  | { type: "gameRequest"; minutes: number; increment: number };
+
 export default function Notifications() {
   const notificationQueue = useRef(new Queue<React.ReactNode>()).current;
   const [visibleNotification, setVisibleNotification] =
     useState<React.ReactNode | null>(null);
   const { updateFriends } = useUserContext();
 
-  const updateVisibleNotification = () => {
+  function updateVisibleNotification() {
     setVisibleNotification(notificationQueue.first?.data ?? null);
-  };
+  }
 
-  const removeNotification = async () => {
+  async function removeNotification() {
     notificationQueue.dequeue();
     updateVisibleNotification();
     await fetch(`${import.meta.env.VITE_BACKEND_URL}/notifications`, {
@@ -27,63 +38,96 @@ export default function Notifications() {
         "Content-Type": "application/json"
       }
     });
-  };
+  }
+
+  function buildNotification(notification: Notification) {
+    let notificationComponent;
+    switch (notification.type) {
+      case "friendRequest":
+        notificationComponent = (
+          <FriendRequestNotification
+            username={notification.from}
+            remove={removeNotification}
+            key={notificationQueue.last ? notificationQueue.last.id : -1}
+          />
+        );
+        break;
+      case "friendAccept":
+        notificationComponent = (
+          <MessageNotification
+            remove={removeNotification}
+            key={notificationQueue.last ? notificationQueue.last.id : -1}
+          >
+            <span>
+              <b>{notification.from}</b> is now your friend
+            </span>
+          </MessageNotification>
+        );
+        updateFriends();
+        break;
+      case "friendDidDecline":
+        notificationComponent = (
+          <MessageNotification
+            remove={removeNotification}
+            key={notificationQueue.last ? notificationQueue.last.id : -1}
+          >
+            <span>
+              You declined <b>{notification.from}</b>'s friend request
+            </span>
+          </MessageNotification>
+        );
+        break;
+      case "friendWasDeclined":
+        notificationComponent = (
+          <MessageNotification
+            remove={removeNotification}
+            key={notificationQueue.last ? notificationQueue.last.id : -1}
+          >
+            <span>
+              <b>{notification.from}</b> declined your friend request
+            </span>
+          </MessageNotification>
+        );
+    }
+
+    return notificationComponent;
+  }
 
   useEffect(() => {
-    socket.on("notification", (notification) => {
-      let notificationComponent;
-      switch (notification.type) {
-        case "friendRequest":
-          notificationComponent = (
-            <FriendRequestNotification
-              username={notification.from}
-              remove={removeNotification}
-              key={notificationQueue.last ? notificationQueue.last.id : -1}
-            />
-          );
-          break;
-        case "friendAccept":
-          notificationComponent = (
-            <MessageNotification
-              remove={removeNotification}
-              key={notificationQueue.last ? notificationQueue.last.id : -1}
-            >
-              <span>
-                <b>{notification.from}</b> is now your friend
-              </span>
-            </MessageNotification>
-          );
-          updateFriends();
-          break;
-        case "friendDidDecline":
-          notificationComponent = (
-            <MessageNotification
-              remove={removeNotification}
-              key={notificationQueue.last ? notificationQueue.last.id : -1}
-            >
-              <span>
-                You declined <b>{notification.from}</b>'s friend request
-              </span>
-            </MessageNotification>
-          );
-          break;
-        case "friendWasDeclined":
-          notificationComponent = (
-            <MessageNotification
-              remove={removeNotification}
-              key={notificationQueue.last ? notificationQueue.last.id : -1}
-            >
-              <span>
-                <b>{notification.from}</b> declined your friend request
-              </span>
-            </MessageNotification>
-          );
-      }
-      if (notificationComponent) {
-        notificationQueue.enqueue(notificationComponent);
+    (async () => {
+      try {
+        socket.on("notification", (notification) => {
+          const notificationComponent = buildNotification(notification);
+          if (notificationComponent) {
+            notificationQueue.enqueue(notificationComponent);
+            updateVisibleNotification();
+          }
+        });
+
+        const response = await fetch(
+          `${import.meta.env.VITE_BACKEND_URL}/notifications`,
+          {
+            method: "GET",
+            credentials: "include",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json"
+            }
+          }
+        );
+        const data = await response.json();
+        const { notifications } = data;
+
+        notificationQueue.clear();
+        for (let noti of notifications) {
+          notificationQueue.enqueue(buildNotification(noti));
+        }
         updateVisibleNotification();
+      } catch (error) {
+        console.error(error);
       }
-    });
+    })();
+
     return () => {
       socket.off("notification");
     };
