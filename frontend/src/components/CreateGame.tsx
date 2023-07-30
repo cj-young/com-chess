1ed8fd8480993953d8143f9fe8645ddc9f48fea9
CJ Young <chrjyoung@gmail.com>
2023-07-29 23:40:00 -0400

    Add input validation to live game creation section

--- frontend/src/components/CreateGame.tsx
+++ frontend/src/components/CreateGame.tsx
@@ -1,6 +1,47 @@
+import { useState, ChangeEvent, FocusEvent, useMemo } from "react";
 import "../styles/CreateGame.scss";
 
 export default function CreateGame() {
+  const [minutes, setMinutes] = useState("10");
+  const [increment, setIncrement] = useState("0");
+  const [opponent, setOpponent] = useState("");
+
+  const canCreate = useMemo<boolean>(() => {
+    return opponent.length > 0 && minutes.length > 0 && increment.length > 0;
+  }, [minutes, increment, opponent]);
+
+  function handleMinutesChange(e: ChangeEvent<HTMLInputElement>) {
+    e.preventDefault();
+    if (/^\d*$/.test(e.target.value)) {
+      setMinutes(e.target.value);
+    }
+  }
+
+  function handleMinutesBlur(e: FocusEvent<HTMLInputElement>) {
+    if (+minutes > 60) setMinutes("60");
+  }
+
+  function handleIncrementChange(e: ChangeEvent<HTMLInputElement>) {
+    e.preventDefault();
+    if (/^\d*$/.test(e.target.value)) {
+      setIncrement(e.target.value);
+    }
+  }
+
+  function handleIncrementBlur(e: FocusEvent<HTMLInputElement>) {
+    if (+increment > 30) setIncrement("30");
+  }
+
+  function handleOpponentChange(newOpponent: string): () => void {
+    return () => {
+      setOpponent(newOpponent);
+    };
+  }
+
+  function handleCreateGame() {
+    if (!canCreate) return;
+  }
+
   return (
     <div className="create-game">
       <h2>Create a game</h2>
@@ -13,6 +54,9 @@ export default function CreateGame() {
             name="minutes"
             inputMode="numeric"
             placeholder="00"
+            value={minutes}
+            onChange={handleMinutesChange}
+            onBlur={handleMinutesBlur}
           />
           <label htmlFor="minutes">Minutes</label>
         </div>
@@ -23,6 +67,9 @@ export default function CreateGame() {
             name="increment"
             inputMode="numeric"
             placeholder="00"
+            value={increment}
+            onChange={handleIncrementChange}
+            onBlur={handleIncrementBlur}
           />
           <label htmlFor="increment">Increment</label>
         </div>
@@ -35,6 +82,8 @@ export default function CreateGame() {
             name="opponent"
             value="Username1"
             id="Username1"
+            checked={opponent === "Username1"}
+            onChange={handleOpponentChange("Username1")}
           />
           <label htmlFor="Username1">Username1</label>
         </div>
@@ -44,6 +93,8 @@ export default function CreateGame() {
             name="opponent"
             value="Username2"
             id="Username2"
+            checked={opponent === "Username2"}
+            onChange={handleOpponentChange("Username2")}
           />
           <label htmlFor="Username2">Username2</label>
         </div>
@@ -53,6 +104,8 @@ export default function CreateGame() {
             name="opponent"
             value="Username3"
             id="Username3"
+            checked={opponent === "Username3"}
+            onChange={handleOpponentChange("Username3")}
           />
           <label htmlFor="Username3">Username3</label>
         </div>
@@ -62,11 +115,18 @@ export default function CreateGame() {
             name="opponent"
             value="Username4"
             id="Username4"
+            checked={opponent === "Username4"}
+            onChange={handleOpponentChange("Username4")}
           />
           <label htmlFor="Username4">Username4</label>
         </div>
       </div>
-      <button>Create Game</button>
+      <button
+        onClick={handleCreateGame}
+        className={canCreate ? "" : "inactive"}
+      >
+        Create Game
+      </button>
     </div>
   );
 }
