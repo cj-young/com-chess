aee30998d2de17db596b6616bfb395f7be3e5296
CJ Young <chrjyoung@gmail.com>
2023-07-31 15:58:48 -0400

    Create live game db model

---
+++ backend/models/LiveGame.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const MoveSchema = new Schema({
+  from: {
+    type: String,
+    required: true
+  },
+  to: {
+    type: String,
+    required: true
+  },
+  promoteTo: {
+    type: String
+  }
+});
+
+const LiveGameSchema = new Schema({
+  moves: {
+    type: [MoveSchema],
+    default: []
+  },
+  blackPlayer: {
+    type: String
+  },
+  whitePlayer: {
+    type: String
+  },
+  blackTime: {
+    type: Number
+  },
+  whiteTime: {
+    type: Number
+  },
+  minutes: {
+    type: Number
+  },
+  increment: {
+    type: Number
+  }
+});
+
+const LiveGame = mongoose.model("LiveGame", LiveGameSchema);
+
+module.exports = LiveGame;
