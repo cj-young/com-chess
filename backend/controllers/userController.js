70c595f8a1cb9fff8cf1db8748bbfbec654c2c47
CJ Young <chrjyoung@gmail.com>
2023-09-17 22:34:47 -0400

    Fix json not including past games correctly

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const { PastLiveGame, PastBotGame } = require("../models/PastGame");
 
 const getProfile = async (req, res, next) => {
   try {
@@ -8,11 +9,71 @@ const getProfile = async (req, res, next) => {
     if (!user) return res.status(404).json({ message: "User not found" });
 
     const isSelf = requester.id === user.id;
+
+    const [liveGames, botGames] = await Promise.all([
+      PastLiveGame.find({
+        $or: [{ blackPlayer: user.id }, { whitePlayer: user.id }],
+      })
+        .sort({ createdAt: -1 })
+        .limit(10),
+      PastBotGame.find({ user: user.id }).sort({ createdAt: -1 }).limit(10),
+    ]);
+
+    const combinedGames = [
+      ...liveGames.map((game) => ({ ...game.toObject(), type: "live" })),
+      ...botGames.map((game) => ({ ...game.toObject(), type: "bot" })),
+    ]
+      .sort((a, b) => b.createdAt - a.createdAt)
+      .slice(0, 10);
+
+    for (let i = 0; i < combinedGames.length; i++) {
+      const game = combinedGames[i];
+      let winStatus;
+
+      if (game.type === "live") {
+        const [whitePlayer, blackPlayer] = await Promise.all([
+          User.findById(game.whitePlayer),
+          User.findById(game.blackPlayer),
+        ]);
+        const [whiteUsername, blackUsername] = [
+          whitePlayer.username,
+          blackPlayer.username,
+        ];
+
+        const color =
+          game.whitePlayer.toString() === user.id ? "white" : "black";
+        combinedGames[i] = {
+          ...combinedGames[i],
+          whiteUsername,
+          blackUsername,
+          color,
+        };
+      }
+
+      if (game.type === "bot") {
+        winStatus = game.winner
+          ? game.winner === game.color
+            ? "won"
+            : "lost"
+          : "drawn";
+      } else {
+        const isWhite = user.id === game.whitePlayer.toString();
+        winStatus = game.winner
+          ? (game.winner === "white" && isWhite) ||
+            (game.winner === "black" && !isWhite)
+            ? "won"
+            : "lost"
+          : "drawn";
+      }
+
+      combinedGames[i] = { ...combinedGames[i], winStatus };
+    }
+
     res.json({
       isSelf,
       joinedAt: user.createdAt,
       username: user.username,
-      pastGames: user.pastGames,
+      pastGames: combinedGames,
       id: user.id,
     });
   } catch (error) {
