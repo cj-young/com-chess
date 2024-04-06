a1981a32c9e559501c55ec6adde63e6032e66265
CJ Young <chrjyoung@gmail.com>
2024-04-05 21:13:48 -0400

    ✨ Add message to loading screen when loading takes long

--- frontend/src/pages/Loading/index.tsx
+++ frontend/src/pages/Loading/index.tsx
@@ -1,6 +1,23 @@
+import { CSSProperties, useEffect, useState } from "react";
 import Spinner from "../../components/Spinner";
 
+const MESSAGE_DELAY = 2500;
+const MESSAGE =
+  "The backend of this app is hosted on a free service, so loading may take up to a minute.";
+
 export default function Loading() {
+  const [showMessage, setShowMessage] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowMessage(true);
+    }, MESSAGE_DELAY);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+
   return (
     <div
       className="callback"
@@ -8,9 +25,26 @@ export default function Loading() {
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
-        height: "100%"
+        height: "100%",
+        flexDirection: "column",
+        gap: "1rem"
       }}
     >
+      {showMessage && (
+        <div
+          style={
+            {
+              textAlign: "center",
+              color: "var(--clr-text-primary)",
+              fontSize: "var(--fs-500)",
+              maxWidth: "50ch",
+              textWrap: "balance"
+            } as CSSProperties
+          }
+        >
+          {MESSAGE}
+        </div>
+      )}
       <Spinner size="2rem" />
     </div>
   );
